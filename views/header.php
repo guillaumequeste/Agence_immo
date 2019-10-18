cca2e9649391796acdfb97c50f4707092fd01f97
Guillaume Queste <guillaume.queste@laposte.net>
2019-10-18 11:30:10 +0200

    liens surbrillance fin

--- views/header.php
+++ views/header.php
@@ -80,8 +80,8 @@ $page = $_REQUEST["page"] ?? "home";
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php?page=home">Accueil <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+              <a class="nav-link <?php if ($_SERVER['QUERY_STRING'] == 'page=home'){echo 'active';}?>" href="index.php?page=home">Accueil <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link <?php if ($_SERVER['QUERY_STRING'] == 'page=contact'){echo 'active';}?>" href="index.php?page=contact">Contact</a>
